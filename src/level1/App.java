a15f507f856736b8242dd70f58f6922d126ec6f8
gminnimk <tvt1245@naver.com>
2024-05-24 20:58:27 +0900

    level1 - 2

--- src/level1/App.java
+++ src/level1/App.java
@@ -23,14 +23,24 @@ public class App {
 
         System.out.println("첫 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        // int 타입의 num1 변수 선언 후 sc.nextInt()로 정수를 입력받게 설정.
         int num1 = sc.nextInt();
 
         System.out.println("두 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        // int 타입의 num2 변수 선언 후 sc.nextInt()로 정수를 입력받게 설정.
         int num2 = sc.nextInt();
 
 
-        System.out.println("결과 :" + num1 + " " + num2);
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        // char 타입의 operator 라는 변수를 선언
+        // sc.next().charAt(0) => 스캐너로 문자열을 입력받고 charAt(0)를 통해 처음 문자를 추출.
+        char operator = sc.next().charAt(0);
+
+
+        // 결과 확인하기 위한 출력문
+        System.out.println("결과 :" + num1 + " " + num2 + " " + operator);
     }
 }
 
