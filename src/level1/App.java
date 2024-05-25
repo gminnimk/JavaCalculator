9ccd87ce82ec2bc40d35d3bbf83fb4486c84ef39
gminnimk <tvt1245@naver.com>
2024-05-26 01:35:02 +0900

    level1 - 4

--- src/level1/App.java
+++ src/level1/App.java
@@ -23,43 +23,55 @@ public class App {
 
         int result = 0;
 
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        // int 타입의 num1 변수 선언 후 sc.nextInt()로 정수를 입력받게 설정.
-        int num1 = sc.nextInt();
 
-        System.out.println("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        // int 타입의 num2 변수 선언 후 sc.nextInt()로 정수를 입력받게 설정.
-        int num2 = sc.nextInt();
+        while (true) { // 반복문을 사용해서 무한 루프
 
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            // int 타입의 num1 변수 선언 후 sc.nextInt()로 정수를 입력받게 설정.
+            int num1 = sc.nextInt();
 
-        System.out.println("사칙연산 기호를 입력하세요: ");
-        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-        // char 타입의 operator 라는 변수를 선언
-        // sc.next().charAt(0) => 스캐너로 문자열을 입력받고 charAt(0)를 통해 처음 문자를 추출.
-        char operator = sc.next().charAt(0);
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            // int 타입의 num2 변수 선언 후 sc.nextInt()로 정수를 입력받게 설정.
+            int num2 = sc.nextInt();
 
 
-        // 조건문을 활용해서 operator가 각 기호를 가질때 result 값을 설정해줌
-        if (operator == '+') {
-            result = num1 + num2;
-        } else if (operator == '-') {
-            result = num1 - num2;
-        } else if (operator == '*') {
-            result = num1 * num2;
-        } else if (operator == '/') {
-            if(num2 == 0){ // 두 번째 입력된 값이 0일때 오류 발생
-                System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            // char 타입의 operator 라는 변수를 선언
+            // sc.next().charAt(0) => 스캐너로 문자열을 입력받고 charAt(0)를 통해 처음 문자를 추출.
+            char operator = sc.next().charAt(0);
+
+
+            // 조건문을 활용해서 operator가 각 기호를 가질때 result 값을 설정해줌
+            if (operator == '+') {
+                result = num1 + num2;
+            } else if (operator == '-') {
+                result = num1 - num2;
+            } else if (operator == '*') {
+                result = num1 * num2;
+            } else if (operator == '/') {
+                if (num2 == 0) { // 두 번째 입력된 값이 0일때 오류 발생
+                    System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                }
+                result = num1 / num2;
+            } else {    // 사칙연산에서 벗어난 기호는 따로 처리
+                System.out.println("기호를 잘못 입력하였습니다.");
             }
-            result = num1 / num2;
-        } else {    // 사칙연산에서 벗어난 기호는 따로 처리
-            System.out.println("기호를 잘못 입력하였습니다."); 
-        }
 
-        // 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력
-        // 결과 확인하기 위한 출력문
-        System.out.println("결과 : " +  result);
+            // exit 문자열 입력시 무한 루프에서 벗어남.
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String str = sc.next();
+            if (str.equals("exit")) {
+                break;
+            }
+
+            // 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력
+            // 결과 확인하기 위한 출력문
+            System.out.println("결과 : " + result);
+        }
     }
 }
 
