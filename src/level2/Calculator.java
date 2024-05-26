7a941a904b180b6cec94e1752b5e9b31ab7c760b
gminnimk <tvt1245@naver.com>
2024-05-26 20:29:19 +0900

    init commit

---
+++ src/level2/Calculator.java
@@ -0,0 +1,23 @@
+package level2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    public static void main(String[] args) {
+        /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
+        List results = new ArrayList<>();
+
+
+        public void calculate(){
+            /* 위 요구사항에 맞게 구현 */
+            /* return 연산 결과 */
+
+        }
+
+   // public 반환타입 calculate(...매개변수) {
+        /* 위 요구사항에 맞게 구현 */
+        /* return 연산 결과 */
+
+    }
+}
