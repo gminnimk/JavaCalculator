9bb73dd9d64f4f85b9a236a9999fad715a9ba7b1
gminnimk <tvt1245@naver.com>
2024-05-26 19:07:11 +0900

    [복습] level1 - 4 / 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다. ■ 숙지 사항

--- src/level1/App.java
+++ src/level1/App.java
@@ -15,6 +15,7 @@ JAVA CALULATOR LV1
 
  */
 
+import java.util.Arrays;
 import java.util.Scanner; // Scanner를 사용하기 위한 import
 
 public class App {
@@ -22,11 +23,17 @@ public class App {
         Scanner sc = new Scanner(System.in); // 양의 정수 2개를 전달 받기 위한 Scanner 선언.
         int result = 0; // 사칙연산에 따라 결과값 반환을 위한 변수 선언.
 
+        int max_results = 10; // 하드코딩 방지.
+        int[] results = new int[max_results]; // 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성.
+        int count = 0; // 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언
+
+
 
         while (true) { // 무한 루프를 위한 while 반복문 선언.
 
             System.out.println("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt(); // num1 변수에 정수 입력받기.
+
 
             System.out.println("두 번째 숫자를 입력하세요: ");
             int num2 = sc.nextInt(); // num2 변수에 정수 입력받기.
@@ -58,6 +65,18 @@ public class App {
 
             // 결과값 확인.
             System.out.println("결과 : " + result);
+
+
+
+
+            results[count] = result; // 연산의 결과를 배열에 저장
+            count ++; // index 증가
+
+            // ▶ 숙지
+            // 배열에 들어간 값을 확인하기 위해서는 results 가 아닌 Arrays.toString(results)와 같이 작성
+            // Arrays를 사용하기 위해서는 import로 라이브러리를 가져와야 함.
+            System.out.println("배열 확인 : " + Arrays.toString(results) + " " + "count : " + count); // 배열에 값이 들어갔는지 확인.
+
 
 
             // exit 문자열 입력시 break를 이용해 무한 루프를 빠져나옴.
