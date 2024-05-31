8ceb389b91527f9f949f7e6be76d4faac7ebf53c
gminnimk <tvt1245@naver.com>
2024-05-31 14:27:09 +0900

    Level2 - 2 (설명추가) / Level 1에서 구현한 App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정합니다

---
+++ src/level2/App.java
@@ -0,0 +1,47 @@
+package level2;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        /* Calculator 인스턴스 생성 */
+        Calculator calc = new Calculator();
+
+        Scanner sc = new Scanner(System.in);
+
+        /* 반복문 시작 */
+        while (true) {
+
+            System.out.print("첫 번째 숫자를 입력하세요:");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요:");
+            int num2 = sc.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+
+            try {
+                // 위에서 입력한 숫자를 Calculator 클래스와 연동하여 실질적으로/
+                // 계산을 수행하는 코드를 구현.
+                // ◆◆◆◆◆ 위 내용에 대한 복습 필요 ◆◆◆◆◆
+                double result = calc.calculate(num1, num2, operator);
+                System.out.println("결과 : " + result);
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+
+
+            /* 반복문 종료 */
+            String exit = sc.next();
+            if (exit.equals("exit")) { //equals를 사용하여 해당 문자열이 입력시 조건문 발동
+                System.out.println("프로그램을 종료합니다.");
+                break; // break로 프로그램 종료
+            }
+        }
+    }
+}
--- src/level2/Calculator.java
+++ src/level2/Calculator.java
@@ -10,6 +10,7 @@
 
 package level2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -27,7 +28,31 @@ public class Calculator {
 
     // 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
     // ◆◆◆◆◆ 위 내용에 대해 복습 필요 ◆◆◆◆◆
-    public List<Double> results;
+    private List<Double> results;
+
+    // public List<Double> results;
+    // 위 결과값을 저장하는 컬렉션은 내부에서만 사용되는 데이터 구조이기 때문에 private으로 선언하는게 올바름
+    // 무조건 자료구조 형태라고 private으로 선언하는게 아니라
+    // 전체적인 구조를 보았을때 그 필드가 클래스 내부에서만 사용되는 경우에 해당.
+    // 이는 필드가 외부에서 직접적인 접근 및 조작을 허용하지 않음을 의미.
+
+    // private 으로 선언하게 되면 => 클래스의 캡슐화를 유지하고, 클래스 간의 의존성을 최소화하여
+    // 유연하고 안전한 코드를 작성하는 데 도움이 됨.
+
+    // 따라서, 클래스의 필드를 'private'으로 선언할 때에는 해당 필드가 외부에서 사용될 필요가 없고, 오로지 클래스
+    // 내부에서만 필요한 데이터인지를 고려해야함.
+
+
+
+
+
+    // main 클래스에서 호출하기 위한 메서드 생성
+    // Calculator 객체가 생성될 때마다 새로운 ArrayList 가 results 필드에 할당되어 초기화 됨.
+    // 이 생성자를 통해 'Calculator' 객체가 생성될 때마다 결과값들을 저장하기 위한 새로운 리스트가 생성되며
+    // 이 리스트는 해당 객체의 수명 동안 결과값을 저장하는 데 사용.
+    public Calculator() {
+        results = new ArrayList<>();
+    }
 
 
     public double calculate(int num1, int num2, char operator) throws CalculatorException {
